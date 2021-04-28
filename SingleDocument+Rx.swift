a7668dd572c5959714f8640fbe250e382a6a2b6b
Maximilian Alexander <max@ditto.live>
2021-04-28 19:14:41 -0400

    adding single query

---
+++ SingleDocument+Rx.swift
@@ -0,0 +1,38 @@
+//
+//  SingleDocument+Rx.swift
+//  RxSwiftDitto
+//
+//  Created by Maximilian Alexander on 4/28/21.
+//
+
+import RxSwift
+import DittoSwift
+
+public typealias DittoDocumentWithLiveQueryEvent = (DittoDocument?, DittoSingleDocumentLiveQueryEvent?)
+
+extension DittoPendingIDSpecificOperation: ReactiveCompatible { }
+
+extension Reactive where Base: DittoPendingIDSpecificOperation {
+
+    public var liveQuery: Observable<DittoDocument?> {
+        return Observable.create { observer in
+            let l = self.base.observe { doc, _ in
+                observer.onNext(doc)
+            }
+            return Disposables.create {
+                l.stop()
+            }
+        }
+    }
+
+    public var liveQueryDocumentsWithEvent: Observable<DittoDocumentWithLiveQueryEvent> {
+        return Observable.create { observer in
+            let l = self.base.observe { doc, event in
+                observer.onNext((doc, event))
+            }
+            return Disposables.create {
+                l.stop()
+            }
+        }
+    }
+}
